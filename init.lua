a2507685a844912e942e01a0a8553d588f7289b2
Crazy Cat <vaggelist305@googlemail.com>
2023-04-15 17:24:30 +0300

    test1

--- init.lua
+++ init.lua
@@ -1,3 +1,16 @@
 --loader goes here
 --check game and possibly make required folders
 --add global security features too
+
+local req = request or syn.request
+local placeId = game.PlaceId
+local githubRepo = "https://raw.githubusercontent.com/crazycatdev/lumen/main/"
+local storedVersion = "prosperity.1"
+local githubVersion  = req({
+    Url = githubRepo .. "lumen.json",
+    Method = "GET"
+})
+
+if githubVersion ~= storedVersion then
+    game:GetService("Players").LocalPlayer:Kick("aaaaa")
+end
