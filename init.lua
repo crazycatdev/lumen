3c84cf5395fbd76012ffeb287d82d57e8442c722
Crazy Cat <93070486+crazycatdev@users.noreply.github.com>
2023-08-29 11:56:59 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,19 +1,28 @@
---loader goes here
---check game and possibly make required folders
---add global security features too
-
 local req = request or syn.request
 local githubRepo = "https://raw.githubusercontent.com/crazycatdev/lumen/main/"
 local storedVersion = "prosperity.1"
-local githubConfigEnc  = req({
+local githubConfig = game:GetService("HttpService"):JSONDecode(req({
     Url = githubRepo .. "lumen.json",
     Method = "GET"
-}).Body
-local githubConfig = game:GetService("HttpService"):JSONDecode(githubConfigEnc)
+}).Body)
 
-if githubConfig.version ~= storedVersion then game:GetService("Players").LocalPlayer:Kick("\nOutdated version. Please visit https://lumenlua.xyz\n")
+local function loadCode()
+    local gameID = game.PlaceId
+    if githubConfig.places[gameID] then
+        local codeURL = githubRepo .. gameID .. ".lua"
+        local code = req({
+            Url = codeURL,
+            Method = "GET"
+        }).Body
+
+        loadstring(code)()
+    else
+        -- Load universal
+    end
 end
 
-if not githubConfig.places[game.PlaceId] then
---Load universal here
- end
+if githubConfig.version ~= storedVersion then
+    game:GetService("Players").LocalPlayer:Kick("\nOutdated version. Please visit https://lumenlua.xyz\n")
+else
+    loadCode()
+end
